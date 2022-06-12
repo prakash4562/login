4c4b214fe38c6f613ac6d94c11affe4b09e6b828
chitranshoo <chitranshoo10081998@gmail.com>
2022-06-13 00:13:51 +0530

    two graph added

--- src/app/performance-report/performance-report.component.ts
+++ src/app/performance-report/performance-report.component.ts
@@ -12,20 +12,20 @@ export class PerformanceReportComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const myChart = new Chart("myChart", {
+    const barChart = new Chart("barChart", {
       type: 'bar',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-          label: '# of Votes',
+          label: 'Vistarak with no Data Entry',
           data: [12, 19, 3, 5, 2, 3],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
@@ -35,7 +35,8 @@ export class PerformanceReportComponent implements OnInit {
             'rgba(153, 102, 255, 1)',
             'rgba(255, 159, 64, 1)'
           ],
-          borderWidth: 1
+          borderRadius: 100,
+          borderWidth: 5
         }]
       },
       options: {
@@ -46,7 +47,56 @@ export class PerformanceReportComponent implements OnInit {
         }
       }
     });
-    console.log(myChart)
+
+    const doughnutChart = new Chart("doughnutChart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: 'My first dataset',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+
+    const pieChart = new Chart("pieChart", {
+      type: 'line',
+      data: {
+        // labels: Utils.months({count: 7}),
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
   }
 
 }
