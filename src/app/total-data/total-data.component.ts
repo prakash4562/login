8d3a4c41ace53ff79efb261e94ce98f76bae5e1a
chitranshoo <chitranshoo10081998@gmail.com>
2022-06-18 15:52:35 +0530

    edit, delete in table

--- src/app/total-data/total-data.component.ts
+++ src/app/total-data/total-data.component.ts
@@ -6,41 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./total-data.component.css']
 })
 export class TotalDataComponent implements OnInit {
+  flag = false
+  count;
 
   total_data = [
     {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
     total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '2.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '3.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '4.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '5.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '6.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '7.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '8.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
-    {count: '1.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
+    {count: '9.', name: 'Yatin', mobile: '8888800000', zila: 'Morena',
       ac_name: 'Joura', mandal: '253; 252; 29', assigned_booth: '1',
       booth_president: '1', general_secretary: '0', bla: '1', member:'1',
       total_panna: '21', panna_samiti_member: '21', panna_pramukh: '1', key_voter: '1'},
@@ -49,6 +51,32 @@ export class TotalDataComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editData(count) {
+    alert("Are you want to edit?")
+    this.flag = true
+    this.count = count
+    document.getElementById('table-form')
+  }
+
+  updateData() {
+    this.flag = false
+    let objIndex = this.total_data.findIndex((obj => obj.count == this.count));
+    const name = (<HTMLInputElement>document.getElementById('name')).value
+    const mobile = (<HTMLInputElement>document.getElementById('mobile')).value
+    const zila = (<HTMLInputElement>document.getElementById('zila')).value
+    const ac_name = (<HTMLInputElement>document.getElementById('ac_name')).value
+
+    this.total_data[objIndex].name = name
+    this.total_data[objIndex].mobile = mobile
+    this.total_data[objIndex].zila = zila
+    this.total_data[objIndex].ac_name = ac_name
+  }
+
+  deleteData(count) {
+    alert("Are you want to delete?")
+    this.total_data.splice(count-1, 1)
   }
 
 }
